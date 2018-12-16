46a137eee1a02bc45105fd34fb2f6f0debd4eea0
Shashikant Kadam <shashikant.123kadam@gmail.com>
2018-12-16 17:38:27 +0530

    Add files via upload

--- Cats.py
+++ Cats.py
@@ -8,8 +8,18 @@ Created on Sun Dec 16 13:01:47 2018
 
 import numpy as np
 from DNN import DNN
-from dnn_app_utils_v3 import *
-train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+import h5py
+
+train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+train_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+train_y = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+test_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+test_y = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    
+train_y = train_y.reshape((1, train_y.shape[0]))
+test_y = test_y.reshape((1, test_y.shape[0]))
 # Reshape the training and test examples 
 train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   # The "-1" makes reshape flatten the remaining dimensions
 test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
@@ -19,7 +29,7 @@ train_x = train_x_flatten/255.
 test_x = test_x_flatten/255.
 dnn = DNN(2,[7,1],train_x,train_y,learning_rate = 0.0075, iterations = 2500)
 dnn.train()
-probs, predictions = dnn.predict(train_x)
+probs, predictions = dnn.predict(test_x)
 
-accuracy = np.sum(predictions == train_y)/train_y.shape[1]
+accuracy = np.sum(predictions == test_y)/test_y.shape[1]
 print("accuracy = ", accuracy)
